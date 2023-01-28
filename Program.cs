9c139d026e73c5a8aa1bb469177d006edf8e9720
ai_nick <alittlejoke@mail.ru>
2023-01-28 10:44:12 +0300

    first test

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿/* Задача 34: Задайте массив заполненный случайными 
+положительными трёхзначными числами. Напишите 
+программу, которая покажет количество чётных 
+чисел в массиве.
+
+[345, 897, 568, 234] -> 2 */
+
+
+void PrintArray(int[] array)
+{
+    for (int i = 1; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+void FillArray(int[] array)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(-9, 10);
+    }
+}
